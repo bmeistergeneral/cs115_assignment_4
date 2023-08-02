39288f3e4ed4e70ab68f5a638616a092500acc43
bmeistergeneral <brecon.morgan@gmail.com>
2023-08-02 14:51:59 -0400

    Completed BaseConversion

--- BaseConversion.java
+++ BaseConversion.java
@@ -22,19 +22,48 @@ public class BaseConversion {
         // Call convert and print the answer
         System.out.println(convert(base10Num, base));
     }
+
+    // This method converts a value from 10 to 15 into strings "a" through "f".
+    public static String numsToLetters(int value) throws IllegalArgumentException {
+        if (value == 10) {
+            return "a";
+        } else if (value == 11) {
+            return "b";
+        } else if (value == 12) {
+            return "c";
+        } else if (value == 13) {
+            return "d";
+        } else if (value == 14) {
+            return "e";
+        } else if (value == 15) {
+            return "f";
+        } else {
+            throw new IllegalArgumentException("Base is greater than 16");
+        }
+    }
+
+
     // -------------------------------------------------
     // Converts a base 10 number to another base.
     // -------------------------------------------------
-    public static String convert(int num, int b)
-    {
+    public static String convert(int num, int b) {
         int quotient;  // the quotient when num is divided by base b
         int remainder; // the remainder when num is divided by base b
         quotient = num / b;
         remainder = num % b;
-        if (quotient == 0) {
-            return ("" + remainder);
+
+        // Code to handle bases above 10.
+        String stringRemainder;
+        if (remainder > 9) {
+            stringRemainder = numsToLetters(remainder);
         } else {
-           return("" + convert(quotient, b) + remainder);
+            stringRemainder = "" + remainder;
+        }
+
+        if (quotient == 0) {
+            return stringRemainder;
+        } else {
+            return ("" + convert(quotient, b) + stringRemainder);
         }
     }
 }
