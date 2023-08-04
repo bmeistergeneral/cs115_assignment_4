d010ea56f11a069369807550c69c7c4faaffc124
bmeistergeneral <brecon.morgan@gmail.com>
2023-08-04 10:40:05 -0400

    Completed step 1 of part 4

--- Fib.java
+++ Fib.java
@@ -9,6 +9,16 @@ public class Fib {
     // Recursively computes fib(n)
     //------------------------------------------------------------------
     public static int fib1(int n) {
-        // Fill in code -- this should look very much like the //mathematical specification
+        // Fill in code -- this should look very much like the
+        // mathematical specification
+        System.out.println("In fib1" + "(" + n + ")");
+        if (n > 1) {
+            return fib1(n - 1) + fib1(n - 2);
+        } else if (n == 1) {
+            return 1;
+        } else if (n == 0) {
+            return 0;
+        }
+        return n;
     }
 }
